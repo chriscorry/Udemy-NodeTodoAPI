a7bdd3e375a69a1a1a582d90bf0ff3b8cc646020
Chris C <31288441+chriscorry@users.noreply.github.com>
2017-10-23 16:12:27 -0700

    Add GET /todos route

--- Server/Server.js
+++ Server/Server.js
@@ -10,17 +10,40 @@ var app = express();
 
 app.use(bodyParser.json());
 
+//
+// POST /todos
+// Add a new TODO
+//
 app.post('/todos', (req, resp) => {
+
+  // Create the new TODO
   var todo = new Todo({
     text: req.body.text
   });
 
+  // ... and save to the database and respond
   todo.save().then((doc) => {
     resp.send(doc);
   }, (err) => {
     resp.status(400).send(err);
   });
 });
+
+
+//
+// GET /todos
+// Retrieve all TODOs
+//
+app.get('/todos', (req, resp) => {
+
+  // Make the db request and respond
+  Todo.find().then((todos) => {
+    resp.send( { todos });
+  }, (err) => {
+    resp.status(400).send(err);
+  });
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000...');
