ce0c1c3e20a7f64aed2c609c01475bfe85cd067b
Chris C <31288441+chriscorry@users.noreply.github.com>
2017-12-19 15:50:43 -0800

    Add tests for DELETE /users/me/token

--- Server/Server.js
+++ Server/Server.js
@@ -11,6 +11,12 @@ var { User }         = require('./Models/User');
 var { authenticate } = require('./Middleware/Authenticate');
 
 
+/****************************************************************************
+ **                                                                        **
+ ** SETUP                                                                  **
+ **                                                                        **
+ ****************************************************************************/
+
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -23,139 +29,11 @@ app.use(bodyParser.json());
 console.log(`Starting server in environment "${envName}"...`);
 
 
-//
-// POST /todos
-// Add a new TODO
-//
-app.post('/todos', (req, resp) => {
-
-  // Create the new TODO
-  var todo = new Todo({
-    text: req.body.text
-  });
-
-  // ... and save to the database and respond
-  todo.save().then((doc) => {
-    resp.send(doc);
-  }, (err) => {
-    resp.status(400).send(err);
-  });
-});
-
-
-//
-// GET /todos
-// Retrieve all TODOs
-//
-app.get('/todos', (req, resp) => {
-
-  // Make the db request and respond
-  Todo.find().then((todos) => {
-    resp.send( { todos });
-  }, (err) => {
-    resp.status(400).send(err);
-  });
-});
-
-
-//
-// GET /todos/:id
-// Retrieve a single TODO
-//
-app.get('/todos/:id', (req, resp) => {
-
-  // Is this a valid ID?
-  var id = req.params.id;
-  if (!ObjectID.isValid(id)) {
-    return resp.status(404).send({ reason: 'Invalid ID' });
-  }
-
-  // Make the db request and respond
-  Todo.findById(id).then((todo) => {
-    if (todo) {
-      // Todo doc was found
-      resp.send({ todo });
-    }
-    else {
-      // Todo doc was NOT found
-      resp.status(404).send({ reason: 'ID not found.' });
-    }
-  }).catch((err) => {
-    // Badness, brah
-    resp.status(400).send(err);
-  });
-});
-
-
-//
-// DELETE /todos/:id
-// Remove a single TODO
-//
-app.delete('/todos/:id', (req, resp) => {
-
-  // Is this a valid ID?
-  var id = req.params.id;
-  if (!ObjectID.isValid(id)) {
-    return resp.status(404).send({ reason: 'Invalid ID' });
-  }
-
-  // Make the db request and respond
-  Todo.findByIdAndRemove(id).then((todo) => {
-    if (todo) {
-      // Todo doc was found
-      resp.send({ todo });
-    }
-    else {
-      // Todo doc was NOT found
-      resp.status(404).send({ reason: 'ID not found.' });
-    }
-  }).catch((err) => {
-    // Badness, brah
-    resp.status(400).send(err);
-  });
-});
-
-
-//
-// PATCH /todos/:id
-// Update a single TODO
-//
-app.patch('/todos/:id', (req, resp) => {
-
-  // Is this a valid ID?
-  var id = req.params.id;
-  if (!ObjectID.isValid(id)) {
-    return resp.status(404).send({ reason: 'Invalid ID' });
-  }
-
-  // Bring over new fields, but ONLY those that are editable
-  var body = _.pick(req.body, ['text', 'completed']);
-
-  // Set our completed time, if appropriate
-  if (_.isBoolean(body.completed) && body.completed) {
-    body.completedAt = new Date().getTime();
-  }
-  else {
-    body.completed = false;
-    body.completedAt = null;
-  }
-
-  // Make the db request and respond
-  Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
-    if (todo) {
-      // Todo doc was found
-      resp.send({ todo });
-    }
-    else {
-      // Todo doc was NOT found
-      resp.status(404).send({ reason: 'ID not found.' });
-    }
-  }).catch((err) => {
-    // Badness, brah
-    resp.status(400).send(err);
-  });
-});
-
+/****************************************************************************
+ **                                                                        **
+ ** USERS API                                                              **
+ **                                                                        **
+ ****************************************************************************/
 
 //
 // POST /users/login
@@ -223,6 +101,152 @@ app.post('/users', (req, resp) => {
   });
 });
 
+
+/****************************************************************************
+ **                                                                        **
+ ** TODOS API                                                              **
+ **                                                                        **
+ ****************************************************************************/
+
+//
+// POST /todos
+// Add a new TODO
+//
+app.post('/todos', authenticate, (req, resp) => {
+
+  // Create the new TODO
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  // ... and save to the database and respond
+  todo.save().then((doc) => {
+    resp.send(doc);
+  }, (err) => {
+    resp.status(400).send(err);
+  });
+});
+
+
+//
+// GET /todos
+// Retrieve all TODOs
+//
+app.get('/todos', authenticate, (req, resp) => {
+
+  // Make the db request and respond
+  Todo.find().then((todos) => {
+    resp.send( { todos });
+  }, (err) => {
+    resp.status(400).send(err);
+  });
+});
+
+
+//
+// GET /todos/:id
+// Retrieve a single TODO
+//
+app.get('/todos/:id', authenticate, (req, resp) => {
+
+  // Is this a valid ID?
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return resp.status(404).send({ reason: 'Invalid ID' });
+  }
+
+  // Make the db request and respond
+  Todo.findById(id).then((todo) => {
+    if (todo) {
+      // Todo doc was found
+      resp.send({ todo });
+    }
+    else {
+      // Todo doc was NOT found
+      resp.status(404).send({ reason: 'ID not found.' });
+    }
+  }).catch((err) => {
+    // Badness, brah
+    resp.status(400).send(err);
+  });
+});
+
+
+//
+// DELETE /todos/:id
+// Remove a single TODO
+//
+app.delete('/todos/:id', authenticate, (req, resp) => {
+
+  // Is this a valid ID?
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return resp.status(404).send({ reason: 'Invalid ID' });
+  }
+
+  // Make the db request and respond
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (todo) {
+      // Todo doc was found
+      resp.send({ todo });
+    }
+    else {
+      // Todo doc was NOT found
+      resp.status(404).send({ reason: 'ID not found.' });
+    }
+  }).catch((err) => {
+    // Badness, brah
+    resp.status(400).send(err);
+  });
+});
+
+
+//
+// PATCH /todos/:id
+// Update a single TODO
+//
+app.patch('/todos/:id', authenticate, (req, resp) => {
+
+  // Is this a valid ID?
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return resp.status(404).send({ reason: 'Invalid ID' });
+  }
+
+  // Bring over new fields, but ONLY those that are editable
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  // Set our completed time, if appropriate
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  }
+  else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  // Make the db request and respond
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+    if (todo) {
+      // Todo doc was found
+      resp.send({ todo });
+    }
+    else {
+      // Todo doc was NOT found
+      resp.status(404).send({ reason: 'ID not found.' });
+    }
+  }).catch((err) => {
+    // Badness, brah
+    resp.status(400).send(err);
+  });
+});
+
+
+/****************************************************************************
+ **                                                                        **
+ ** MAIN LOOP                                                              **
+ **                                                                        **
+ ****************************************************************************/
 
 app.listen(port, () => {
     console.log(`Started on port ${port}...`);
--- Server/Tests/Server.test.js
+++ Server/Tests/Server.test.js
@@ -9,8 +9,226 @@ const { todos, populateTodos, users, populateUsers }
                    = require('./Seed/Seed');
 
 
+/****************************************************************************
+ **                                                                        **
+ ** SETUP                                                                  **
+ **                                                                        **
+ ****************************************************************************/
+
+// Setup database before each test
 beforeEach(populateUsers);
 beforeEach(populateTodos);
+
+
+/****************************************************************************
+ **                                                                        **
+ ** USERS API                                                              **
+ **                                                                        **
+ ****************************************************************************/
+
+//
+// POST /users/login
+// Login an existing user
+//
+describe('POST /users/login', () => {
+
+  it('should login an existing user', (done) => {
+
+    var email = users[0].email;
+    var password = users[0].password;
+    var id = users[0]._id;
+
+    request(app)
+      .post('/users/login')
+      .send( {email, password} )
+      .expect(200)
+      .expect((resp) => {
+        expect(resp.headers['x-auth']).toExist();
+        expect(resp.body._id).toExist();
+        expect(resp.body.email).toBe(email);
+      })
+      .end((err, resp) => {
+        if (err) {
+          return done(err);
+        }
+
+        // Verify that the in the database has the same token
+        User.findById(id).then((user) => {
+          expect(user).toExist();
+          expect(user.tokens[1]).toInclude({
+            access: 'auth',
+            token: resp.headers['x-auth']
+          });
+          done();
+        }).catch((err) => done(err));
+      });
+  });
+
+  it('should return error if user does not exist', (done) => {
+
+    var email = 'not an email';
+    var password = users[0].password;
+
+    request(app)
+      .post(`/users/login`)
+      .send( {email, password} )
+      .expect(400)
+      .end(done);
+  });
+
+  it('should return error if incorrect password is specified', (done) => {
+
+    var email = users[0].email;
+    var password = 'notadoctor';
+
+    request(app)
+      .post(`/users/login`)
+      .send( {email, password} )
+      .expect(400)
+      .end(done);
+  });
+
+});
+
+
+//
+// DELETE /users/me/token
+// Logout current user
+//
+describe('DELETE /users/me/token', () => {
+
+  it('should remove auth token on logout', (done) => {
+
+    var id = users[0]._id;
+
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, resp) => {
+        if (err) {
+          return done(err);
+        }
+
+        // Verify that the in the database has the same token
+        User.findById(id).then((user) => {
+          expect(user).toExist();
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((err) => done(err));
+      });
+  });
+
+  it('should return error if user not logged in', (done) => {
+
+    request(app)
+      .delete(`/users/me/token`)
+      .send()
+      .expect(401)
+      .end(done);
+  });
+
+});
+
+
+//
+// GET /users/me
+// Retrieve info about current logged in user
+//
+describe('GET /users/me', () => {
+
+  it('should return a user if authenticated', (done) => {
+
+    request(app)
+      .get(`/users/me`)
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((resp) => {
+        expect(resp.body._id).toBe(users[0]._id.toHexString());
+        expect(resp.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return 401 if not authenticated', (done) => {
+
+    request(app)
+      .get(`/users/me`)
+      .expect(401)
+      .end(done);
+  });
+
+  it('should return 401 if bad authentication token', (done) => {
+
+    request(app)
+      .get(`/users/me`)
+      .set('x-auth', 'notadoctor')
+      .expect(401)
+      .end(done);
+  });
+
+});
+
+
+//
+// POST /users
+// Add a new USER
+//
+describe('POST /users', () => {
+
+  it('should create a user', (done) => {
+
+    var email = 'example@example.com';
+    var password = 'notadoctor';
+
+    request(app)
+      .post('/users')
+      .send( {email, password} )
+      .expect(200)
+      .expect((resp) => {
+        expect(resp.headers['x-auth']).toExist();
+        expect(resp.body._id).toExist();
+        expect(resp.body.email).toBe(email);
+      })
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+
+        // Verify that the user was placed into the database
+        User.findOne({email}).then((user) => {
+          expect(user).toExist();
+          expect(user.password).toNotBe(password);
+          done();
+        }).catch((err) => done(err));
+      });
+  });
+
+  it('should return validation errors if request invalid', (done) => {
+
+    var email = 'not an email';
+    var password = 'notadoctor';
+
+    request(app)
+      .post(`/users`)
+      .send( {email, password} )
+      .expect(400)
+      .end(done);
+  });
+
+  it('should not create user if email already in use', (done) => {
+
+    var email = users[0].email;
+    var password = 'notadoctor';
+
+    request(app)
+      .post(`/users`)
+      .send( {email, password} )
+      .expect(400)
+      .end(done);
+  });
+
+});
 
 
 //
@@ -118,6 +336,12 @@ describe('GET /todos/:id', () => {
 
 });
 
+
+/****************************************************************************
+ **                                                                        **
+ ** TODOS API                                                              **
+ **                                                                        **
+ ****************************************************************************/
 
 //
 // DELETE /todos/:id
@@ -238,171 +462,6 @@ describe('PATCH /todos/:id', () => {
     request(app)
       .delete(`/todos/123`)
       .expect(404)
-      .end(done);
-  });
-
-});
-
-
-//
-// POST /users/login
-// Login an existing user
-//
-describe('POST /users/login', () => {
-
-  it('should login an existing user', (done) => {
-
-    var email = users[0].email;
-    var password = users[0].password;
-    var id = users[0]._id;
-
-    request(app)
-      .post('/users/login')
-      .send( {email, password} )
-      .expect(200)
-      .expect((resp) => {
-        expect(resp.headers['x-auth']).toExist();
-        expect(resp.body._id).toExist();
-        expect(resp.body.email).toBe(email);
-      })
-      .end((err, resp) => {
-        if (err) {
-          return done(err);
-        }
-
-        // Verify that the in the database has the same token
-        User.findById(id).then((user) => {
-          expect(user).toExist();
-          expect(user.tokens[1]).toInclude({
-            access: 'auth',
-            token: resp.headers['x-auth']
-          });
-          done();
-        }).catch((err) => done(err));
-      });
-  });
-
-  it('should return error if user does not exist', (done) => {
-
-    var email = 'not an email';
-    var password = users[0].password;
-
-    request(app)
-      .post(`/users/login`)
-      .send( {email, password} )
-      .expect(400)
-      .end(done);
-  });
-
-  it('should return error if incorrect password is specified', (done) => {
-
-    var email = users[0].email;
-    var password = 'notadoctor';
-
-    request(app)
-      .post(`/users/login`)
-      .send( {email, password} )
-      .expect(400)
-      .end(done);
-  });
-
-});
-
-
-//
-// GET /users/me
-// Retrieve info about current logged in user
-//
-describe('GET /users/me', () => {
-
-  it('should return a user if authenticated', (done) => {
-
-    request(app)
-      .get(`/users/me`)
-      .set('x-auth', users[0].tokens[0].token)
-      .expect(200)
-      .expect((resp) => {
-        expect(resp.body._id).toBe(users[0]._id.toHexString());
-        expect(resp.body.email).toBe(users[0].email);
-      })
-      .end(done);
-  });
-
-  it('should return 401 if not authenticated', (done) => {
-
-    request(app)
-      .get(`/users/me`)
-      .expect(401)
-      .end(done);
-  });
-
-  it('should return 401 if bad authentication token', (done) => {
-
-    request(app)
-      .get(`/users/me`)
-      .set('x-auth', 'notadoctor')
-      .expect(401)
-      .end(done);
-  });
-
-});
-
-
-//
-// POST /users
-// Add a new USER
-//
-describe('POST /users', () => {
-
-  it('should create a user', (done) => {
-
-    var email = 'example@example.com';
-    var password = 'notadoctor';
-
-    request(app)
-      .post('/users')
-      .send( {email, password} )
-      .expect(200)
-      .expect((resp) => {
-        expect(resp.headers['x-auth']).toExist();
-        expect(resp.body._id).toExist();
-        expect(resp.body.email).toBe(email);
-      })
-      .end((err) => {
-        if (err) {
-          return done(err);
-        }
-
-        // Verify that the user was placed into the database
-        User.findOne({email}).then((user) => {
-          expect(user).toExist();
-          expect(user.password).toNotBe(password);
-          done();
-        }).catch((err) => done(err));
-      });
-  });
-
-  it('should return validation errors if request invalid', (done) => {
-
-    var email = 'not an email';
-    var password = 'notadoctor';
-
-    request(app)
-      .post(`/users`)
-      .send( {email, password} )
-      .expect(400)
-      .end(done);
-  });
-
-  it('should not create user if email already in use', (done) => {
-
-    var email = users[0].email;
-    var password = 'notadoctor';
-
-    request(app)
-      .post(`/users`)
-      .send( {email, password} )
-      .expect(400)
       .end(done);
   });
 
